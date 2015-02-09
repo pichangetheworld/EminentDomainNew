32368551db41086915f857d7e43bced5e89ec934
pchan <pichangetheworld@gmail.com>
2015-02-09 22:35:34 +0900

    Actually adding the select mode.

---
+++ app/src/main/java/com/pichangetheworld/eminentdomainnew/util/SelectMode.java
@@ -0,0 +1,12 @@
+package com.pichangetheworld.eminentdomainnew.util;
+
+/**
+ * Eminent Domain AS
+ * Author: pchan
+ * Date: 09/02/2015
+ */
+public enum SelectMode {
+    ACTION_PLAY_PHASE,
+    MATCH_ROLE_PHASE,
+    SELECT_TO_DISCARD_DRAW_PHASE
+}
